e34cb6f3bc20312ba65f6eea1e0dc165d9604ec0
Joel Peter <joelapeter82@gmail.com>
2025-01-25 19:32:28 +0530

    Fixed Layout and Analytics

--- Client/src/layout/Layout.jsx
+++ Client/src/layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
 import NavBar from './Navbar';
+import { Outlet } from 'react-router-dom';
 
-export const Layout = ({ children }) => {
+export const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
       <main className="flex-grow">
-        {children}
+        <Outlet />
       </main>
       <Footer />
     </div>
